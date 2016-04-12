8a21913d229b3f8ea6960efa9648a5bb5d2adfc7
Louis Gesbert <louis.gesbert@ocamlpro.com>
2016-04-12 18:36:47 +0900

    Add an opam2 clone synchronised to the next branch

--- update-from-git.sh
+++ update-from-git.sh
@@ -45,6 +45,16 @@ case $repo in
             cd ..
         done
         ;;
+    "opam2")
+        ./configure -prefix ~/local
+        make
+        make -C doc html man-html pages
+        mkdir -p ~/local/share/doc/2.0
+        cp -r doc/html ~/local/share/doc/2.0/api
+        cp -r doc/pages ~/local/share/doc/2.0/manual
+        make -C admin-scripts 1_2_to_2_0
+        mv admin-scripts/1_2_to_2_0 ~/local/bin/repo_1_2_to_2_0
+        ;;
     "opamfu")
         make uninstall || true
         make build
